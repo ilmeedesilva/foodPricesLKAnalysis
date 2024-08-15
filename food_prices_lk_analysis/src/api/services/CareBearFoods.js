5d1587e391c150810038d3da4b1607b3ebb77fbe
difelk <dilshan.f@theavo.net>
2024-08-15 07:03:55 +0530

    ui improvement V2.3

--- food_prices_lk_analysis/src/api/services/CareBearFoods.js
+++ food_prices_lk_analysis/src/api/services/CareBearFoods.js
@@ -16,6 +16,21 @@ const CareBearFoods = {
       console.log("error in get all csv data : ", e);
     }
   },
+
+  // create necessary functions in here
+  // You only need to create your endpoint function (in this case, it's an object here).
+  // SERVICETYPE.CARE_BEAR_SERVICE is a constant; don’t change it. It must be included in the every function call.
+
+  // example post request
+
+  // functionName: async (data) => { //when you call this pass necessary value
+  //   try {
+  //     return await apiUtils.put(`modal/kmean`, data, SERVICETYPE.CARE_BEAR_SERVICE);
+  //   } catch (e) {
+  //     console.log("error in put  users : ", e);
+  //   }
+  // },
+  // },
 };
 
 export default CareBearFoods;
